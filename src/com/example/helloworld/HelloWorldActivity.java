55cac80ab293a473b52b09685ec551066298218f
Administrator <Administrator@7A304-0007>
2016-12-07 11:10:20 +0800

    TAB框以及TAB页面转换

--- src/com/example/helloworld/HelloWorldActivity.java
+++ src/com/example/helloworld/HelloWorldActivity.java
@@ -1,17 +1,73 @@
 package com.example.helloworld;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import fragments.MainTabbarFragment;
+import fragments.MainTabbarFragment.OnTabSelectedListener;
+import page.FeedListFragment;
+import page.MyProfileFragment;
+import page.NoteListFragment;
+import page.SearchPageFragment;
+
 /**
- * @author 刘世杰
- * HelloWorld Activity
- * 点击登陆后弹出的一个界面,并没有什么卵用----应该后面要做什么这个就改成什么吧
+ * @author 刘世杰 HelloWorld Activity 点击登陆后弹出的一个界面,并没有什么卵用----应该后面要做什么这个就改成什么吧
  */
 public class HelloWorldActivity extends Activity {
+	FeedListFragment contentFeedList = new FeedListFragment();
+	NoteListFragment contentNoteList = new NoteListFragment();
+	SearchPageFragment contentSearchPage = new SearchPageFragment();
+	MyProfileFragment contentMyProfile = new MyProfileFragment();
+
+	MainTabbarFragment tabbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_helloworld);
-		
+
+		tabbar = (MainTabbarFragment) getFragmentManager().findFragmentById(R.id.frag_tabbar);
+		tabbar.setOnTabSelectedListener(new OnTabSelectedListener() {
+
+			@Override
+			public void onTabSelected(int index) {
+				changeContentFragment(index);
+			}
+		});
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		tabbar.setSelectedItem(0);
+	}
+
+	void changeContentFragment(int index) {
+		Fragment newFrag = null;
+
+		switch (index) {
+		case 0:
+			newFrag = contentFeedList;
+			break;
+		case 1:
+			newFrag = contentNoteList;
+			break;
+		case 2:
+			newFrag = contentSearchPage;
+			break;
+		case 3:
+			newFrag = contentMyProfile;
+			break;
+
+		default:
+			break;
+		}
+
+		if (newFrag == null)
+			return;
+
+		getFragmentManager().beginTransaction().replace(R.id.content, newFrag).commit();
 	}
 }
---
+++ src/fragments/MainTabbarFragment.java
@@ -0,0 +1,75 @@
+package fragments;
+
+import com.example.helloworld.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainTabbarFragment extends Fragment {
+	
+	View btFree,tabFeeds,tabNotes,tabSearch,tabMe;
+	View[] tabs;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_main_tabbar, null);
+		
+		btFree = view.findViewById(R.id.bt_free);
+		tabFeeds = view.findViewById(R.id.tab_Feeds);
+		tabNotes = view.findViewById(R.id.tab_Notes);
+		tabSearch = view.findViewById(R.id.tab_Search);
+		tabMe = view.findViewById(R.id.tab_Me);
+		
+		tabs = new View[]{
+				tabFeeds,tabNotes,tabSearch,tabMe
+		};
+		
+		for(final View tab : tabs){
+			tab.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					onTabClicked(tab);
+				}
+			});
+		}
+		return view;
+	}
+	
+	public static interface OnTabSelectedListener{
+		void onTabSelected(int index);
+	}
+	
+	OnTabSelectedListener onTabSelectedListener;
+	
+	public void setOnTabSelectedListener(OnTabSelectedListener onTabSelectedListener){
+		this.onTabSelectedListener = onTabSelectedListener;
+	}
+	
+	public void setSelectedItem(int index){
+		if(index>=0 && index<tabs.length){
+			onTabClicked(tabs[index]);
+		}
+	}
+	
+	void onTabClicked(View tab){
+		
+		int selectedIndex = -1;
+		for (int i=0;i<tabs.length;i++){
+			View otherTab = tabs[i];
+			if(otherTab==tab){
+				otherTab.setSelected(true);
+				selectedIndex = i;
+			}else{
+				otherTab.setSelected(false);
+			}
+		}
+		
+		if(onTabSelectedListener!=null && selectedIndex>=0){
+			onTabSelectedListener.onTabSelected(selectedIndex);
+		}
+	}
+}
